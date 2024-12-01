ab85e5bb7175a35d63aa9e392ac59f27e6898580
ChincholeGayatri <chincholegayatri31@gmail.com>
2024-12-01 13:53:59 +0530

    new files adding

--- template.py
+++ template.py
@@ -4,10 +4,10 @@ import logging
 
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s] : %(message)s')
 
-project_name = "textsummarizer"
+project_name = "textsummarizer"  # Define the project name here
 
 list_of_files = [
-    ".github./workflows/.gitkeep"
+    ".github/workflows/.gitkeep",  # Corrected path
     f"src/{project_name}/__init__.py",
     f"src/{project_name}/components/__init__.py",
     f"src/{project_name}/utils/__init__.py",
@@ -18,15 +18,16 @@ list_of_files = [
     f"src/{project_name}/pipeline/__init__.py",
     f"src/{project_name}/entity/__init__.py",
     f"src/{project_name}/constants/__init__.py",
-    "config/config.yaml",
-    "params.yaml",
+    "config/config.yaml",  
+    "params.yaml",         
     "app.py",
     "main.py",
     "Dockerfile",
     "requirements.txt",
     "setup.py",
-    "research/trials.ipynb"
+    "research/trials.ipynb",
 ]
+
 
 for filepath in list_of_files:
     filepath = Path(filepath)
